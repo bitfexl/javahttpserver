5d22bf96d8719c8ea9d2ba06915fb9df0a454b88
bitfexl <54662051+bitfexl@users.noreply.github.com>
2022-07-10 12:29:54 +0200

    refactor

--- src/main/java/com/github/bitfexl/httpserver/Method.java
+++ src/main/java/com/github/bitfexl/httpserver/Method.java
@@ -6,11 +6,10 @@ public enum Method {
     GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH, UNKNOWN;
 
     public static Method fromString(String rawMethod) {
-        for(Method method : new Method[]{ GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH }) {
-            if(rawMethod.equalsIgnoreCase(method.toString())) {
-                return method;
-            }
+        try {
+            return Method.valueOf(rawMethod.toUpperCase());
+        } catch (Exception ex) {
+            return UNKNOWN;
         }
-        return UNKNOWN;
     }
 }
